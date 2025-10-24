7f682d70ef47655ab0f029e40a1d6d2014e157b8
GustavoSilva11620 <shark11620@sharkcoders.pt>
2025-10-24 20:05:35 +0100

    Add files via upload

---
+++ pintar parede.py
@@ -0,0 +1,13 @@
+comprimento=float(input("Qual é o comprimento da parede?"))
+largura=float(input("E a largura?"))
+print("o perimetro da parede é ",comprimento+largura,"m")
+area=(comprimento*largura)
+print("a área da parede é ",area,"m^2")
+print("serão necessários ",area / 2,"litros")
+valor=round(area/2,2)
+print("serão necessários",valor*15,"euros de tinta" )
+
+
+
+
+
---
+++ pintar parede2.py
@@ -0,0 +1,8 @@
+comprimento=int(input("Qual é o comprimento da parede?"))
+largura=int(input("E a largura?"))
+area=(comprimento*largura)
+litros= area/2
+valor=litros*15
+print("Area da parede é:", area,"\nO valor para pintar é:")
+
+
